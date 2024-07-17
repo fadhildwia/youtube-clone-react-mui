cba303f7f9781c204347f661e0a1c1240a3878a0
Fadhil <fadhilahdwi.a@gmail.com>
2024-07-17 11:18:59 +0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { ChannelDetail, Feed, SearchFeed, VideoDetail } from './pages'
+import { Navbar } from './components'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/SearchFeed.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchFeed: React.FC = () => {
+  return (
+    <div>SearchFeed</div>
+  )
+}
+
+export default SearchFeed
