997a9e3f818316a77ce375631dedf9ffbe6e0b55
Fadhil <fadhilahdwi.a@gmail.com>
2024-07-19 10:12:11 +0700

    add meta data

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Box } from '@mui/material'
 import { ChannelDetail, Feed, SearchFeed, VideoDetail } from './pages'
 import { Navbar } from './components'
+import { Helmet } from 'react-helmet'
 
 function App() {
   return (
     <BrowserRouter>
+      <Helmet>
+        <meta name="description" content="This is a Youtube Clone built with React and Vite" />
+        <title>Youtube Clone</title>
+      </Helmet>
       <Box sx={{ backgroundColor: '#000' }}>
         <Navbar />
         <Routes>
